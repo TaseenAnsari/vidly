2cb44b4fa1f1b056bd60d3ca4c8a054fba281352
TaseenAnsari <taseensaif@gmal.com>
2021-11-26 19:19:48 +0530

    pot

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express();
 require('./starter/start')(app,express);
 
-const PORT = process.env.PORT |8333;
+const PORT = process.env.PORT || 8333;
 app.get('/',(req ,res)=>{
     res.send("hello")
 })
+
 server = app.listen(PORT,()=>{ console.log("connected to localhost@",PORT)})
 
 module.exports = server
